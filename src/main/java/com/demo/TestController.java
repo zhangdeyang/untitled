c9e004b4d6f1fcf3e9a8b8de31e2c9646fb2645b
zdy <610413558@qq.com>
2023-07-18 22:28:32 +0800

    呃呃

---
+++ src/main/java/com/demo/TestController.java
@@ -0,0 +1,15 @@
+package com.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @RequestMapping(value = {"/test"},method = RequestMethod.GET)
+    public String hello(){
+        return "hello world 亲爱的小孩";
+    }
+
+}
